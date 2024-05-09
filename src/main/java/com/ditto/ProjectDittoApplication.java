93a30d672746ec324881d4230ddcfc834184266d
koreavc <koreavc@DESKTOP-78A5593>
2024-05-09 16:59:34 +0900

    hello ditto

---
+++ src/main/java/com/ditto/ProjectDittoApplication.java
@@ -0,0 +1,13 @@
+package com.ditto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDittoApplication.class, args);
+	}
+
+}
